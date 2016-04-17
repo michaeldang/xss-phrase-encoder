27fd4ecc4ad3f73e004d8460a2079e857e2e3b53
Ivy Wong <ivy.cm.wong@gmail.com>
2016-04-17 03:31:21 -0700

    Fire toast when one of the encoding options is clicked.

--- js/contentModifier.js
+++ js/contentModifier.js
@@ -1,11 +1,36 @@
+var charCodeArea = "charCodeEncodingArea";
+var htmlEncodingArea = "htmlEncodingArea";
+var uriEncodingArea = "uriEncodingArea";
+var hexUEncodingArea = "hexUEncodingArea";
+var hexXEncodingArea = "hexXEncodingArea";
+var base64EncodingArea = "base64EncodingArea";
+
 $(document).ready(function() {
+    setFieldsToReadOnly();
+    
     $('#originalText').on('input', function() {
-    var text = document.getElementById("originalText").value;
-    alterText(text);
+        var text = document.getElementById("originalText").value;
+        alterText(text);
     });
+        
+    copyValue();
 });
 
+function setFieldsToReadOnly() {
+    $("#" + charCodeArea).prop("readonly",true);
+    $("#" + htmlEncodingArea).prop("readonly",true);
+    $("#" + uriEncodingArea).prop("readonly",true);
+    $("#" + hexUEncodingArea).prop("readonly",true);
+    $("#" + hexXEncodingArea).prop("readonly",true);
+    $("#" + base64EncodingArea).prop("readonly",true);
+}
 
+function copyValue() {
+    $("textarea[id*='EncodingArea']").click(function() {
+        var id = $(this).attr("id").match(/.+?(?=EncodingArea)/);                    
+        Materialize.toast(id + " value copied to clickboard", 2000); 
+    });
+}
 
 function alterText(text) {
     var letters = text.split('');
@@ -16,12 +41,12 @@ function alterText(text) {
     var xhexText = buildXHexEncodedPhrase(letters);
     var base64Text = buildBase64Phrase(text);
     
-    setFieldValToPhrase("charCodeArea", charCodeText);
-    setFieldValToPhrase("htmlEncodingArea", htmlText);
-    setFieldValToPhrase("uriEncodingArea", uriText);
-    setFieldValToPhrase("hexUEncodingArea", uhexText);
-    setFieldValToPhrase("hexXEncodingArea", xhexText);
-    setFieldValToPhrase("base64EncodingArea", base64Text);
+    setFieldValToPhrase(charCodeArea, charCodeText);
+    setFieldValToPhrase(htmlEncodingArea, htmlText);
+    setFieldValToPhrase(uriEncodingArea, uriText);
+    setFieldValToPhrase(hexUEncodingArea, uhexText);
+    setFieldValToPhrase(hexXEncodingArea, xhexText);
+    setFieldValToPhrase(base64EncodingArea, base64Text);
 }
 
 function setFieldValToPhrase(fieldName, phrase) {
