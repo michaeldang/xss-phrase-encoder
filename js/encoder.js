785c8797a20f0e9640b7e9c87fa8cb0d6cdaa232
Michael Dang <michael.w.dang@gmail.com>
2016-04-16 21:29:29 -0700

    Added hexadecimal encoding

--- js/encoder.js
+++ js/encoder.js
@@ -6,8 +6,16 @@ function letterToHTML(letter) {
     var charCode = letterToCharCode(letter);
     var numZeros = 5 - charCode.toString().length;
     var charCodeWithZeros =  addZerosToPhrase(numZeros, charCode);
-    var decimalEncoding = "&#" + charCodeWithZeros + ";";
-    return decimalEncoding;
+    var htmlEncoding = "&#" + charCodeWithZeros + ";";
+    return htmlEncoding;
+}
+
+function letterToHex(letter) {
+    var hexCode = letterToCharCode(letter).toString(16);
+    var numZeros = 4 - hexCode.length;
+    var hexCodeWithZeros = addZerosToPhrase(numZeros, hexCode);
+    var hexEncoding = "\\u" + hexCodeWithZeros;
+    return hexEncoding;
 }
 
 function addZerosToPhrase(numZeros, phrase) {
